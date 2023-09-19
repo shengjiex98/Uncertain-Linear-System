e88c2568fe9ae40cd4d7552c02b22ac24ec04380
shengjiex98 <shengjiex98@gmail.com>
2023-09-19 10:01:03 -0400

    Add benchmark and starting code

--- src/MultiStates.py
+++ src/MultiStates.py
@@ -3,6 +3,11 @@ PROJECT_ROOT = os.environ['ULS_ROOT_DIR']
 sys.path.append(PROJECT_ROOT)
 
 from lib.OrderUncertainties import *
+import numpy as np
+import control as ct
+import sympy
+from sympy import symbols
+from Benchmarks import sys_variables
 
 def get_sensitivity(A, B):
     mat = OrdUnc(A)
@@ -14,3 +19,15 @@ def get_sensitivity(A, B):
 
 def all_sensitivity(A):
     pass
+
+def find_AB(sys, x):
+    nx = sys.A.shape[0]
+    l = symbols('l')
+    E = np.eye(nx)
+    E[x,x] = l
+    print(nx)
+
+def main():
+    find_AB("")
+
+main()
