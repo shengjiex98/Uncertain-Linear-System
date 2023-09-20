cca9fa8ed2a7d8f18b977bfad4c6a723b79ef31e
Shengjie Xu <shengjiex98@gmail.com>
2023-09-20 14:56:59 -0400

    Merge

--- src/MultiStates.py
+++ src/MultiStates.py
@@ -1,13 +1,21 @@
 import os,sys
 PROJECT_ROOT = os.environ['ULS_ROOT_DIR']
+
 sys.path.append(PROJECT_ROOT)
 
-from lib.OrderUncertainties import *
 import numpy as np
-import control as ct
-import sympy
-from sympy import symbols
+import control
+from sympy import symbols, Matrix
+from lib.OrderUncertainties import *
 from Benchmarks import sys_variables
+
+class Bench:
+    def __init__(self, A, B, C=None, D=None) -> None:
+        self.A = A
+        self.B = B
+        self.nx = A.shape[1]
+        self.nu = B.shape[1]
+        self.C = C if C else np.eye
 
 def get_sensitivity(A, B):
     mat = OrdUnc(A)
@@ -17,8 +25,11 @@ def get_sensitivity(A, B):
         unc = mat.multSig(B)
     return unc
 
-def all_sensitivity(A):
-    pass
+def all_sensitivity(sys, K):
+    # A_bar_i = A + BKE_i where E_i = I(n) with 
+    for i in range(sys.nx):
+        E_i = Matrix(np.eye(sys.nx))
+        E_i[i, i] = symbols('l')
 
 def find_AB(sys, x):
     nx = sys.A.shape[0]
