4c091d8518a0532e09482371f308f837dd17099e
shengjiex98 <shengjiex98@gmail.com>
2023-09-26 23:32:24 -0400

    Results

---
+++ efficientnet-surface.jl
@@ -0,0 +1,11 @@
+# Accuracy (top1/top5) in percentage, parameter in millions, FLOP in billions
+efficient_net_surfaces = (
+    B0 = (acc1 = 77.1, acc5 = 93.3, para = 5.3, flop = 0.39),
+    B1 = (acc1 = 79.1, acc5 = 94.4, para = 7.8, flop = 0.70),
+    B2 = (acc1 = 80.1, acc5 = 94.9, para = 9.2, flop = 1.0),
+    B3 = (acc1 = 81.6, acc5 = 95.7, para = 12, flop = 1.8),
+    B4 = (acc1 = 82.9, acc5 = 96.4, para = 19, flop = 4.2),
+    B5 = (acc1 = 83.6, acc5 = 96.7, para = 30, flop = 9.9),
+    B6 = (acc1 = 84.0, acc5 = 96.8, para = 43, flop = 19),
+    B7 = (acc1 = 84.3, acc5 = 97.0, para = 66, flop = 37),
+)
